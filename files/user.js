763e825a6f7545fd6702720a597d76c185a2886f
xyr365 <66541662+xyr365@users.noreply.github.com>
2022-02-20 19:18:43 +0800

    Update user.js

--- files/user.js
+++ files/user.js
@@ -1,1 +1,1 @@
-setInterval(document.getElementsByTagName("div").setAttribute('style','cfont-size: large; margin: 10px; color: rgb(29, 149, 63);'),50)
+setInterval($response.body.document.getElementsByTagName("div").setAttribute('style','cfont-size: large; margin: 10px; color: rgb(29, 149, 63);'),50)
