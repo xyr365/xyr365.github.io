993116b51850c750a767e1edcb856d648157324c
xyr365 <66541662+xyr365@users.noreply.github.com>
2022-02-20 18:34:27 +0800

    Update New Userscript.user.js

--- files/New Userscript.user.js
+++ files/New Userscript.user.js
@@ -1,11 +1,1 @@
-// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://ykt.fzu.edu.cn:9001/ThirdWeb/PayCode
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.
-// @grant        none
-// ==/UserScript==
 setInterval(document.getElementById("currentTime").setAttribute('style','cfont-size: large; margin: 10px; color: rgb(29, 149, 63);'),50)
